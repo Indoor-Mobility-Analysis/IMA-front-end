18332979919a577d19f36c5954f21d916123dfbf
EmilyXu1018 <xuhaodi19941018@gmail.com>
2017-05-05 18:10:23 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import VueVideoPlayer from 'vue-video-player'
+import 'vue-awesome/icons'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
+import Icon from 'vue-awesome/components/Icon'
 // Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(VueVideoPlayer);
+Vue.component('icon', Icon);
 
 /* eslint-disable no-new */
 new Vue({
