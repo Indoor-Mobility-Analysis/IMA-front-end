156c8d898a7ca1d0e127c3d20b210a81da599a5f
EmilyXu1018 <xuhaodi19941018@gmail.com>
2017-05-05 18:04:55 +0800

    Update VideoAnalysis.vue

--- src/components/VideoAnalysis.vue
+++ src/components/VideoAnalysis.vue
@@ -1,23 +1,154 @@
 <template>
   <div class="video-analysis-container">
-    This is {{title}}
+   <div class = "wrapper">
+      <div class ="nav-logo">
+         <icon class = "logo" name="play-circle" scale = "2" color="#fff"></icon>
+      </div>
+       <div class = "nav-selection">
+           <p>{{title}}</p>
+       </div>
+   </div>
+    <div class = "video-player-1">
+       <video-player  ref="videoPlayer" :options="playerOptions"></video-player>
+    </div>
+    <div class = "video-player-2">
+       <video-player ref="videoPlayer" :options="playerOptions2"></video-player>
+    </div>
+    <div class ="video-player-3">
+      <video-player ref="videoPlayer" :options="playerOptions3"></video-player>
+    </div>
+    <div style = "display:none"></div>
   </div>
 </template>
 
 <script>
+  import bus from "../assets/eventBus";
   export default {
     name: 'VideoAnalysis',
     data(){
       return {
-        title: 'Video Analysis Component'
+           title:'Video Analysis',
+           playerOptions: {
+                start: 0,
+                playsinline: false,
+                muted: true,
+                language: 'en',
+                height:150,
+                autoplay:true,
+                playbackRates: [0.7, 1.0, 1.5, 2.0],
+                sources: [{
+                  type: "video/mp4",
+                  src: "/static/video/sample.mp4"
+                }],
+           },
+           playerOptions2:{
+               start:0,
+               playsinline:false,
+               muted:true,
+               language:'en',
+               playbackRates:[0.7, 1.0, 1.5, 2.0],
+               height:150,
+               autoplay:true,
+               sources: [{
+                  type: "video/mp4",
+                  src: "/static/video/sample.mp4"
+                }],
+           },
+           playerOptions3:{
+               start:0,
+               playsinline:false,
+               muted:true,
+               language:'en',
+               playbackRates:[0.7, 1.0, 1.5, 2.0],
+               height:152,
+               autoplay:true,
+               sources: [{
+                  type: "video/mp4",
+                  src: "/static/video/sample.mp4"
+                }],
+           }
       }
+    },
+    mounted(player){
+        var self = this;
+        bus.$on("event1",function(){
+            self.playerOptions.sources = [{
+                  type: "video/mp4",
+                  src: "/static/video/motion.mp4"
+                }];
+            self.playerOptions2.sources = [{
+                  type: "video/mp4",
+                  src: "/static/video/motion.mp4"
+                }];
+            self.playerOptions3.sources  = [{
+                  type: "video/mp4",
+                  src: "/static/video/motion.mp4"
+                }];
+        });
+        bus.$on("event2",function(){
+           self.playerOptions.sources = [{
+                  type: "video/mp4",
+                  src: "/static/video/sample.mp4"
+                }];
+            self.playerOptions2.sources = [{
+                  type: "video/mp4",
+                  src: "/static/video/sample.mp4"
+                }];
+            self.playerOptions3.sources  = [{
+                  type: "video/mp4",
+                  src: "/static/video/sample.mp4"
+                }];
+        });
+        bus.$on("event3",function(){
+           self.playerOptions.sources = [{
+                  type: "video/mp4",
+                  src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
+                }];
+             self.playerOptions2.sources = [{
+                  type: "video/mp4",
+                  src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
+                }];
+              self.playerOptions3.sources  = [{
+                  type: "video/mp4",
+                  src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
+                }];
+      });
     }
   }
 </script>
 
 <style>
   .video-analysis-container{
-    background-color: #7574b6;
+    background-color: #FFF;
     /*height: 100%*/
+  }
+  .wrapper{
+    display:-webkit-box;
+    background-color:#9CC;
+    height:40px;
+  }
+   .nav-logo{
+    height:50px;
+  }
+  .nav-selection{
+    padding:10px;
+    height:50px;
+    color:#fff;
+    font-weight:bold;
+  }
+  .video-player-1{
+    height:150px;
+    width:100%;
+    background-color:#fff;
+  }
+  .video-player-2{
+    height:150px;
+    width:100%;
+    background-color:#ccc;
+  }
+  .video-player-3{
+    height:152px;
+    width:100%;
+    background-color:#fff;
   }
 </style>
