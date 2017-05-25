1642cff3fd04fb73359cf36b9459462553f2483d
HaipengZeng <tsanghaipeng@gmail.com>
2017-05-25 23:38:19 +0800

    remove bubble map

--- src/components/VisualizationComponents/StationMap.vue
+++ src/components/VisualizationComponents/StationMap.vue
@@ -47,7 +47,8 @@
         _this.frameData = _this.parseFrame(frame);
         if(_this.stationMap){
           _this.stationMap.updateHeatmapCanvas(_this.frameData);
-          _this.stationMap.updateBubblemap(_this.frameData);
+          console.log('_this.frameData: ', _this.frameData)
+          // _this.stationMap.updateBubblemap(_this.frameData);
         }
       });
       pipeService.onFloorSelected(function(floor){
