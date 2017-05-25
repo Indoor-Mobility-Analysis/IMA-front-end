865b29a0b512bf6060f658725254cf5c3adf4801
HaipengZeng <tsanghaipeng@gmail.com>
2017-05-25 23:23:09 +0800

    add shadow on each floor box for trend view

--- src/lib/StationTrend.js
+++ src/lib/StationTrend.js
@@ -11,37 +11,28 @@ let StationTrend = function(el){
   this.now = new Date(Date.now() - this.duration);
 
   // need to adjust later, perhaps based on data
-  this.groups = {
-    current: {
-      index: 0,
-      value: 0,
-      color: 'orange',
-      data: d3.range(this.limit).map(function(){
-        return 0;
-      })
+  this.groups = [
+    {
+      'floor': 0,
+      'color':'orange', 
+      'data': d3.range(this.limit).map(function(){return 0;})
     },
-    target: {
-      index: 1,
-      value: 0,
-      color: 'green',
-      data: d3.range(this.limit).map(function(){
-        return 0;
-      })
+    {
+      'floor': -1,
+      'color':'green', 
+      'data': d3.range(this.limit).map(function(){return 0;})
     },
-    output: {
-      index: 2,
-      value: 0,
-      color: 'grey',
-      data: d3.range(this.limit).map(function(){
-        return 0;
-      })
-    },
-  };
-};
+    {
+      'floor': -2,
+      'color':'grey', 
+      'data': d3.range(this.limit).map(function(){return 0;})
+    }
+  ]
+}
 
 StationTrend.prototype.initContainer = function(){
   let _this = this;
-  this.margin = {top: 20, left: 30, right:20 ,bottom: 20};
+  this.margin = {top: 30, left: 40, right:30 ,bottom: 30};
   this.width = this.$el.clientWidth - this.margin.left - this.margin.right;
   this.height = this.$el.clientHeight - this.margin.top - this.margin.bottom;
 
@@ -50,13 +41,16 @@ StationTrend.prototype.initContainer = function(){
 
   this.floorHeight = (this.heightPerSvg - this.margin.bottom) / 3;
 
+  let floorTopMargin = 30;
+  let floorBottomMargin = 30;
+
   this.xScale = d3.scaleTime()
     .domain([this.now - (this.limit-1)*this.duration, this.now])
     .range([0, this.width]);
 
   this.yScale = d3.scaleLinear()
     .domain([0, 200])
-    .range([this.floorHeight - 35, 0]);
+    .range([this.floorHeight - floorTopMargin - floorBottomMargin, 0]);
 
   this.line = d3.line()
     .x(function(d, i) {
@@ -75,146 +69,100 @@ StationTrend.prototype.initContainer = function(){
     .attr('width', this.widthPerSvg)
     .attr('height', this.heightPerSvg)
 
-  this.gTop = this.svg.append('g')
-    .attr('class', 'top')
-    .attr('transform', 'translate(' + (this.margin.left)+','+((this.floorHeight*0)+15) + ')')
+  this.initShadow();
+  
+  this.gList = [];
+  this.xList = [];
+  this.yList = [];
+  this.barList = [];
+  this.pathList = [];
+  for(let idx=0; idx<this.groups.length; idx++){
 
-  this.gMiddle = this.svg.append('g')
-    .attr('class', 'middle')
-    .attr('transform', 'translate(' + (this.margin.left)+','+((this.floorHeight*1)+15) + ')')
+    let rectTmp = this.svg.append('g')
+      .append('rect').attr('x', 0)
+      .attr('y',0)
+      .attr('width',this.widthPerSvg)
+      .attr('height', this.floorHeight-floorTopMargin/2)
+      .attr('opacity', 0.5)
+      .attr('fill', '#f0f0f0')
+      .style("filter", "url(#drop-shadow)")
+      .attr('transform', 'translate(' +0+','+((this.floorHeight*idx)+floorTopMargin/4) + ')');
 
-  this.gBottom = this.svg.append('g')
-    .attr('class', 'bottom')
-    .attr('transform', 'translate(' + (this.margin.left)+','+((this.floorHeight*2)+15) + ')')
+    let gTmp = this.svg.append('g')
+      .attr('class', 'g idx_'+idx)
+      .attr('transform', 'translate(' + (this.margin.left)+','+((this.floorHeight*idx)+floorTopMargin) + ')');
 
-  this.xTop = this.gTop.append('g')
-    .attr('class', 'x axis top')
-    .attr('transform', 'translate(0,' + (this.floorHeight-35) + ')')
-    .call(d3.axisBottom(this.xScale))
+    let xTmp = gTmp.append('g')
+      .attr('class', 'x axis idx_'+idx)
+      .attr('transform', 'translate(0,' + (this.floorHeight-floorTopMargin-floorBottomMargin) + ')')
+      .call(d3.axisBottom(this.xScale));
 
-  this.xMiddle = this.gMiddle.append('g')
-    .attr('class', 'x axis middle')
-    .attr('transform', 'translate(0,' + (this.floorHeight-35) + ')')
-    .call(d3.axisBottom(this.xScale))
+    let yTmp = gTmp.append('g')
+      .attr('class', 'y axis idx_'+idx)
+      .attr('transform', 'translate(0,' + 0 + ')')
+      .call(d3.axisLeft(this.yScale));
 
-  this.xBottom = this.gBottom.append('g')
-    .attr('class', 'x axis bottom')
-    .attr('transform', 'translate(0,' + (this.floorHeight-35) + ')')
-    .call(d3.axisBottom(this.xScale))
-
-  this.yTop = this.gTop.append('g')
-    .attr('class', 'y axis top')
-    .attr('transform', 'translate(0,' + 0 + ')')
-    .call(d3.axisLeft(this.yScale))
-
-  this.yMiddle = this.gMiddle.append('g')
-    .attr('class', 'y axis middle')
-    .attr('transform', 'translate(0,' + 0 + ')')
-    .call(d3.axisLeft(this.yScale))
-
-  this.yBottom = this.gBottom.append('g')
-    .attr('class', 'y axis bottom')
-    .attr('transform', 'translate(0,' + 0 + ')')
-    .call(d3.axisLeft(this.yScale))
-
-  this.xList = [this.xTop, this.xMiddle, this.xBottom];
-  this.yList = [this.yTop, this.yMiddle, this.yBottom];
-
-  this.barTop = this.gTop.append("line")
-    .attr("x1", this.width)  //<<== change your code here
-    .attr("y1", 0)
-    .attr("x2", this.width)  //<<== and here
-    .attr("y2", this.floorHeight-35)
-    .style("stroke-width", 1)
-    .style("stroke", "red")
-    .style("fill", "none");
-
-  this.barMiddle = this.gMiddle.append("line")
-    .attr("x1", this.width)  //<<== change your code here
-    .attr("y1", 0)
-    .attr("x2", this.width)  //<<== and here
-    .attr("y2", this.floorHeight-35)
-    .style("stroke-width", 1)
-    .style("stroke", "red")
-    .style("fill", "none");
-
-  this.barBottom = this.gBottom.append("line")
-    .attr("x1", this.width)  //<<== change your code here
-    .attr("y1", 0)
-    .attr("x2", this.width)  //<<== and here
-    .attr("y2", this.floorHeight-35)
-    .style("stroke-width", 1)
-    .style("stroke", "red")
-    .style("fill", "none");
-
-  this.paths = [this.gTop.append('g'), this.gMiddle.append('g'), this.gBottom.append('g')];
-
-  for (var name in this.groups) {
-    var group = this.groups[name]
-    group.path = this.paths[group.index].append('path')
+    let barTmp = gTmp.append("line")
+      .attr("x1", this.width)  //<<== change your code here
+      .attr("y1", 0)
+      .attr("x2", this.width)  //<<== and here
+      .attr("y2", this.floorHeight-floorTopMargin-floorBottomMargin)
+      .style("stroke-width", 1)
+      .style("stroke", "red")
+      .style("fill", "none");
+    
+    var group = this.groups[idx];
+    group.path = gTmp.append('g').append('path')
       .data([group.data])
-      .attr('class', name + ' group')
+      .attr('class', group.floor)
       .attr('fill', 'none')
       .style('stroke', group.color)
+    
+    this.gList.push(gTmp);
+    this.xList.push(xTmp);
+    this.yList.push(yTmp);
+    this.barList.push(barTmp);
+    this.pathList.push(group.path);
   }
 };
 
 
 StationTrend.prototype.updateData = function(frameData){
   this.now = new Date();
-  let idx = 0;
   // Remove oldest data point from each group
-  for (var name in this.groups) {
-    var group = this.groups[name];
+  for(let idx=0; idx<this.groups.length; idx++){
+    var group = this.groups[idx];
     group.data.shift()
-  }
-
-    // Add new values
-  for (var name in this.groups) {
-    var group = this.groups[name]
-
-    // need to change to corresponding floor id, currently hard code.
-
     let floor = frameData['records'][idx];
     if(!floor) continue
     let ppl_cnt = floor['ppl_cnt'];
     group.data.push(ppl_cnt);
-    ++idx;
   }
-
 }
 
 
 StationTrend.prototype.updateLinechart = function (frameData){
+  console.log('frameData: ', frameData);
   let _this = this;
   this.now = new Date();
-  let idx = 0;
   let yMax = 0;
   // Remove oldest data point from each group
-  for (var name in this.groups) {
-    var group = this.groups[name];
+  for(let idx=0; idx<this.groups.length; idx++){
+    var group = this.groups[idx];
     group.data.shift()
-  }
 
-  // Add new values
-  for (var name in this.groups) {
-    var group = this.groups[name]
-
-    // need to change to corresponding floor id, currently hard code.
     let floor = frameData['records'][idx];
     if(!floor) continue
     let ppl_cnt = floor['ppl_cnt'];
     group.data.push(ppl_cnt);
     yMax = d3.max(group.data) > yMax ? d3.max(group.data):yMax;
-    ++idx;
   }
 
   this.xScale.domain([this.now - (this.limit-1) * this.duration, this.now]);
   this.yScale.domain([0, yMax+30]);
 
-  idx = 0;
-  for (var name in this.groups) {
-    var group = this.groups[name]
+  for(let idx=0; idx<this.groups.length; idx++){
+    var group = this.groups[idx]
     // Shift domain
     this.xList[idx].transition()
       .duration(this.duration)
@@ -230,14 +178,47 @@ StationTrend.prototype.updateLinechart = function (frameData){
     });
     group.path.attr('d', this.line);
 
-    this.paths[idx].transition()
+    this.pathList[idx].transition()
     .duration(this.duration)
     .ease(d3.easeLinear)
     .attr('transform', 'translate(' + (this.xScale(this.now-this.duration)-this.xScale(this.now)) + ')')
-
-    idx += 1;
   }
 }
 
+StationTrend.prototype.initShadow = function(){
+
+  var defs = this.svg.append("defs");
+
+// create filter with id #drop-shadow
+// height=130% so that the shadow is not clipped
+  var filter = defs.append("filter")
+    .attr("id", "drop-shadow")
+    .attr("height", "130%");
+
+// SourceAlpha refers to opacity of graphic that this filter will be applied to
+// convolve that with a Gaussian with standard deviation 3 and store result
+// in blur
+  filter.append("feGaussianBlur")
+    .attr("in", "SourceAlpha")
+    .attr("stdDeviation", 5)
+    .attr("result", "blur");
+
+// translate output of Gaussian blur to the right and downwards with 2px
+// store result in offsetBlur
+  filter.append("feOffset")
+    .attr("in", "blur")
+    .attr("dx", 5)
+    .attr("dy", 5)
+    .attr("result", "offsetBlur");
+
+// overlay original SourceGraphic over translated blurred opacity by using
+// feMerge filter. Order of specifying inputs is important!
+  var feMerge = filter.append("feMerge");
+
+  feMerge.append("feMergeNode")
+    .attr("in", "offsetBlur")
+  feMerge.append("feMergeNode")
+    .attr("in", "SourceGraphic");
+}
 
 export default StationTrend
